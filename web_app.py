84693452ba30203c5e38a3e43406715c8b54f1bf
adityayadav3009 <76039270+adityayadav3009@users.noreply.github.com>
2022-04-20 00:36:03 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -19,7 +19,7 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import seaborn as sns
 from textblob import TextBlob
-
+nltk.download('stopwords')
 lemm = WordNetLemmatizer()
 stop_nltk = stopwords.words("english")
 stop_nltk.remove('not')
